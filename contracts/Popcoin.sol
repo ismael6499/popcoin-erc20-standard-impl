7f55a9b1bb6e97a7f7fee9dda8581b8ce5557dbc
Agustin <agustin.acosta@eestn1ee.edu.ar>
2025-11-19 01:10:17 -0300

    'first coin created'

---
+++ contracts/Popcoin.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LPGL-3.0-only
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Popcoin is ERC20 {
+
+    constructor(string memory name_, string memory symbol_) ERC20(name_,symbol_){
+        _mint(msg.sender, 1000 * 1e18);
+    }
+
+}
